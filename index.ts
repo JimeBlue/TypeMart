e62adff7d106933ac636e0087aafb9a601ef6661
Jimena <jimeblue69@gmail.com>
2022-02-25 12:50:27 +0100

    Add logics showing message for preOrder products

--- index.ts
+++ index.ts
@@ -4,4 +4,8 @@ const productName : string = "fanny pack";
 
 const product = products.filter(product => product.name === productName)[0];
 
-console.log(product);
+if (product.preOrder === true) {
+    console.log("We will send you a message when your product is on its way.")
+  }
+
+  console.log(product);
