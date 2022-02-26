d8ffeaa40776a212d8e9d2aaed5442f9403aabca
Jimena <jimeblue69@gmail.com>
2022-02-26 11:20:01 +0100

    Calculate tax and total price

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var products_1 = require("./products");
 var productName = "fanny pack";
 var product = products_1["default"].filter(function (product) { return product.name === productName; })[0];
 var shipping;
+var taxPercent;
+var taxTotal;
+var total;
+var shippingAdress = "Elsenheimerstrasse 100, Munich, Bayern";
 if (product.preOrder === true) {
     console.log("We will send you a message when your product is on its way.");
 }
@@ -11,6 +15,15 @@ if (product.price === 25 || product.price > 25) {
     shipping = 0;
     console.log("This product has free shipping");
 }
-else if (product.price > 25) {
+else {
     shipping = 5;
 }
+if (shippingAdress.match("Bayern")) {
+    taxPercent = .05;
+}
+else {
+    taxPercent = 0.1;
+}
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+console.log(total);
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import products from './products';
 
 const productName : string = "fanny pack";
-
 const product = products.filter(product => product.name === productName)[0];
-
 let shipping : number;
+let taxPercent : number;
+let taxTotal : number;
+let total : number;
+const shippingAdress : string = "Elsenheimerstrasse 100, Munich, Bayern";
 
 if (product.preOrder === true) {
     console.log("We will send you a message when your product is on its way.")
@@ -13,7 +15,18 @@ if (product.preOrder === true) {
   if (product.price === 25 || product.price > 25) {
     shipping = 0;
     console.log("This product has free shipping")
-  } else if (product.price > 25) {
+  } else {
     shipping = 5;
-   
   }
+
+  if (shippingAdress.match("Bayern")) {
+    taxPercent = .05;
+  }else {
+    taxPercent = 0.1;
+  }
+
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+
+console.log(total);
+  
